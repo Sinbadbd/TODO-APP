91ba001f79c7755f42ebca60da4e76ca119c263d
sinbad <sinbad@sinbads-Mac.local>
2018-10-26 09:55:43 -0200

    add button

--- IOSDEV/Costom/GDButton.swift
+++ IOSDEV/Costom/GDButton.swift
@@ -11,18 +11,22 @@ import UIKit
 class GDButton: UIButton {
     
     var title:String!
+    var type:ButtonOption!
+    
     
     init(title: String = "Button", frame:CGRect = .zero, type:ButtonOption = .roundedText) {
         super.init(frame: frame)
         if frame == .zero {
             self.translatesAutoresizingMaskIntoConstraints = false
         }
+        self.title = title
+        self.type = type
+        self.phaseTwo()
         
         
         switch type {
         case .roundedText:
-            self.title = title
-            phaseTwo()
+            roundedText()
             break
         case .squareIcon:
             break
@@ -32,10 +36,18 @@ class GDButton: UIButton {
     }
     
     
+    func roundedText(){
+        self.layer.cornerRadius = 18
+    }
+    func squareIcon(){
+        if let titeleLabel = self.titleLabel {
+            titeleLabel.font = UIFont(name: "OpenSans-Regular.ttf" , size: 24)
+        }
+    }
+    
     func phaseTwo(){
         self.setTitleColor(.grayZero, for: .normal)
         self.setTitle(self.title, for: .normal)
-        self.layer.cornerRadius = 18
         self.backgroundColor = .white
         if let titeleLabel = self.titleLabel {
             titeleLabel.font = UIFont(name: "OpenSans-Regular.ttf" , size: 16)
