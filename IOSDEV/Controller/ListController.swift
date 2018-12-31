f9bce0b6435977e8c2e7de54aae734ceb601facc
sinbad <sinbad@sinbads-Mac.local>
2019-01-01 00:37:01 +0600

    1. Build a Custom UITableView

--- IOSDEV/Controller/ListController.swift
+++ IOSDEV/Controller/ListController.swift
@@ -53,7 +53,7 @@ class ListController: UIViewController , GDHeaderDeletegate, NewItemPopupDelegat
         
         view.addSubview(bg)
         bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        bg.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        bg.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
         bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
         bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         
