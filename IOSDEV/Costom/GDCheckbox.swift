0f105354d8e715232409e8b7a98ecd89f1d437c3
sinbad <sinbad@sinbads-mac.local>
2019-01-05 00:48:53 +0600

    - How to Use Custom Icons In your Apps = Section 4, Lecture 47 - How to Toggle the CheckBox PT 2 = Section 4, Lecture 48

--- IOSDEV/Costom/GDCheckbox.swift
+++ IOSDEV/Costom/GDCheckbox.swift
@@ -14,15 +14,25 @@ class GDCheckbox: UIButton {
     var toggled: Bool? {
         didSet {
             if let toggled = toggled {
-                if toggled {
-                    backgroundColor = .green
-                } else {
-                    backgroundColor = .clear
-                }
+                UIView.animate(withDuration: 0.2, animations: {
+                    if toggled {
+                        self.backgroundColor = .green
+                        self.setImage(UIImage(named: "done-icon"), for: .normal)
+                    } else {
+                        self.backgroundColor = .clear
+                        self.setImage(UIImage(), for: .normal)
+                    }
+                })
             }
         }
     }
     
+    @objc func toggleStatus(){
+        if let status = toggled {
+            toggled = !status
+//            print(toggled)
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -30,6 +40,8 @@ class GDCheckbox: UIButton {
          
         layer.borderColor = UIColor.gray.cgColor
         layer.borderWidth = 1
+        
+        addTarget(self, action: #selector(toggleStatus), for: .touchUpInside)
     }
     
     required init?(coder aDecoder: NSCoder) {
